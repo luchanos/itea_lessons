5a243c1afc2eb411aaa9566585e2c6a8524b892e
luchanos <lukash_91@mail.ru>
2021-06-12 18:43:06 +0300

    [upd] 12 lesson

---
+++ flow_28_04_2021/lesson_12/cw_live.py
@@ -0,0 +1,44 @@
+# s = {(1, 2, 3, 3), (1, 2, 3, 4, 5), {1: 2}}
+# print(s)
+
+# a = [1, 2, 3]
+# b = [3, 4, 5]
+# c = [a, b]
+# print(c)
+# a[0] = 100
+# print(c)
+# c[0][0] = 100
+# print(c)
+
+# a = [1, 2, 0, 3, 4]
+# b = a[:3]
+# print(b)
+# a[1] = 'test'
+# print(b)
+# b = a
+# a[0] = 'test'
+# print(b)
+
+# a = 1
+# b = 2
+# c = 3
+# d = 4
+
+# l = [a, b, c, d]  # [1, 2, 3, 4]
+# print(l)
+# a = 'test'
+# print(l)
+
+
+l = [1, 2, 3, 4]
+
+# for el in l:
+#     print(el)
+
+it = iter(l)
+print(it)
+# try:
+#     while True:
+#         print(next(it))
+# except:
+#     pass
