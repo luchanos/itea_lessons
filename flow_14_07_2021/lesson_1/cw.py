7f59e8aaaec6584e68f8ca3c7eaea7bdd5086692
SrGrey <avargin.s@gmail.com>
2021-07-17 01:34:26 +0100

    Lesson_01_homework_04
    first commit. homework 04 resolution posted

--- flow_14_07_2021/lesson_1/cw.py
+++ flow_14_07_2021/lesson_1/cw.py
@@ -10,6 +10,35 @@
 ранее сумме и после этого завершить программу.
 """
 
+Домашнее задание 4.
+
+def sum_int(string):
+    '''Function return summa of numbers of introduced string
+    and validate characters'''
+    is_valid = True
+    summa = 0
+    for i in range(len(string)):
+        try:
+            string[i] = int(string[i])
+            summa += string[i]
+        except:
+            print('non-integer introduced')
+            is_valid = False
+            break
+    return summa, is_valid
+
+summa = 0
+is_valid = True #Triger for while loop, checking non integer input
+while is_valid:
+    users_input = input("Input numbers separated by Space and press Enter: ")
+    input_as_list = users_input.split()
+    input_result = sum_int(input_as_list)
+    summa += input_result[0]
+    is_valid = input_result[1]
+    print(f'Summa is: {summa}')
+else:
+    print('The programm is finished')
+
 
 # a = 1
 # print(type(a))
