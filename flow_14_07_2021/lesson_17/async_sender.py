8967655b3d6bbfd246625fb771f7780d6a61e3af
luchanos <lukash_91@mail.ru>
2021-09-07 10:06:32 +0300

    [upd] lesson_17 - async sender

---
+++ flow_14_07_2021/lesson_17/async_sender.py
@@ -0,0 +1,45 @@
+import asyncio
+import aio_pika
+
+from logging import getLogger, StreamHandler
+import sys
+
+logger = getLogger(__name__)
+
+stdout_handler = StreamHandler(sys.stdout)
+logger.addHandler(stdout_handler)
+logger.setLevel("DEBUG")
+
+
+class SenderAsync:
+    def __init__(self, url):
+        self.url = url
+        self.connection = None
+
+    async def setup(self, ):
+        self.connection = await aio_pika.connect_robust(
+            self.url
+        )
+
+    async def send(self, routing_key, message):
+        # если добавить эту строчку, то всё зависнет
+        # async with self.connection:
+        channel = await self.connection.channel()
+        async with channel:
+            await channel.default_exchange.publish(
+                aio_pika.Message(body=message.encode()),
+                routing_key=routing_key,
+            )
+        logger.info("sended")
+
+
+async def main():
+    sender = SenderAsync("amqp://rmquser:rmqpass@127.0.0.1/")
+    await sender.setup()
+    await sender.send(routing_key="test_queue", message="123")
+    await sender.send(routing_key="test_queue", message="123")
+    await sender.send(routing_key="test_queue", message="123")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
